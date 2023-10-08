6f3f8c70a7f9c35067df476ac7cf311c408ba03e
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-10-08 17:11:58 -0300

    Atualização de Modulos

--- Javascript/01-Modulo/01-atividade.js
+++
@@ -1,6 +0,0 @@
-var p = 5;
-var resultado
-
-resultado = p <=1 ? 1 : p * ( p - 3 );
-console.log(resultado);
-
--- Javascript/01-Modulo/03-atividade.js
+++
@@ -1,8 +0,0 @@
-let teste = {
-  one:1,
-  two:2,
-  three:3
-}
-for(let p in teste){
-  console.log(p)
-}
